e85b0b8588d4000235234b284516b45aea4978d7
Mnr <info@mnrtechnologies.com>
2025-06-20 16:22:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -589,7 +589,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://pm-ai-tool.vercel.app"
+    "https://studentaitutor.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
